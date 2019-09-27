bd621f9f5fae0bedbb290fe91d782d77c078a3a4
Litetook <litetook@gmail.com>
2019-09-27 10:28:11 +0300

    change some comments

--- clear_unneeded_lines.ps1
+++ clear_unneeded_lines.ps1
@@ -1,7 +1,5 @@
 #This script remove $line_name from all txt files in current folder!
 
-
-
 $script_dir = $PSScriptRoot  #Script launch folder
 $file_names = (get-childitem -Path "$script_dir").name | select-string -pattern "txt"  #Highlight all txt files from $script_dir
 $line_name = "Не удалось получить Учетную запись для отправки" #line to delete 
